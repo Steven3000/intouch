ddbe01e10ad13287886548d11ebdb52b066e84c5
Harsha Murthy <harshamurthy2@gmail.com>
2015-03-03 12:29:00 -0600

    Use resource :routes in place of the manually written routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,24 @@ Rails.application.routes.draw do
 
   namespace :admin do
     # Routes for the Admin Song resource
-    get('/songs', { :controller => 'songs', :action => 'index' })
-    get('/songs/new', { :controller => 'songs', :action => 'new' })
-    post('/songs', { :controller => 'songs', :action => 'create' })
-    get('/songs/:id', { :controller => 'songs', :action => 'show' })
-    get('/songs/:id/edit', { :controller => 'songs', :action => 'edit' })
-    patch('/songs/:id', { :controller => 'songs', :action => 'update' })
-    delete('/songs/:id', { :controller => 'songs', :action => 'destroy' })
+    resource :songs
+    # get('/songs', { :controller => 'songs', :action => 'index' })
+    # get('/songs/new', { :controller => 'songs', :action => 'new' })
+    # post('/songs', { :controller => 'songs', :action => 'create' })
+    # get('/songs/:id', { :controller => 'songs', :action => 'show' })
+    # get('/songs/:id/edit', { :controller => 'songs', :action => 'edit' })
+    # patch('/songs/:id', { :controller => 'songs', :action => 'update' })
+    # delete('/songs/:id', { :controller => 'songs', :action => 'destroy' })
 
     # Routes for the Admin Artist resource
-    get('/artists', { :controller => 'artists', :action => 'index' })
-    get('/artists/new', { :controller => 'artists', :action => 'new' })
-    post('/artists', { :controller => 'artists', :action => 'create' })
-    get('/artists/:id', { :controller => 'artists', :action => 'show' })
-    get('/artists/:id/edit', { :controller => 'artists', :action => 'edit' })
-    patch('/artists/:id', { :controller => 'artists', :action => 'update' })
-    delete('/artists/:id', { :controller => 'artists', :action => 'destroy' })
+    resource :artists
+    # get('/artists', { :controller => 'artists', :action => 'index' })
+    # get('/artists/new', { :controller => 'artists', :action => 'new' })
+    # post('/artists', { :controller => 'artists', :action => 'create' })
+    # get('/artists/:id', { :controller => 'artists', :action => 'show' })
+    # get('/artists/:id/edit', { :controller => 'artists', :action => 'edit' })
+    # patch('/artists/:id', { :controller => 'artists', :action => 'update' })
+    # delete('/artists/:id', { :controller => 'artists', :action => 'destroy' })
   end
 
   # Songs Public
