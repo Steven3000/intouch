d2cfac14b3f7cc672407b193b376b7d4f7c2f40a
Steven Callen <wchi_2000@yahoo.com>
2015-03-13 12:13:11 -0500

    fix main feed and others

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,6 @@
 class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
+    render layout: "feed"
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,7 @@ class SongsController < ApplicationController
 
   def index
     @songs = Song.all.order(release_date: :desc)
+    render layout: "feed"
   end
 
   def show
