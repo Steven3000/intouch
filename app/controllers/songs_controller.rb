33f1238e3d7af9818982ad9bb8f196b519e552c4
Steven Callen <wchi_2000@yahoo.com>
2015-05-03 14:10:15 -0500

    Main feed

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,4 +10,16 @@ class SongsController < ApplicationController
     render layout: "feed_layout"
   end
 
+
+private
+
+
+    def set_song
+      @song = Song.find(params[:id])
+    end
+
+    def songs_params
+      params.require(:song).permit(:artist_id, :song_id, :title, :features, :producer, :album_title, :release_date, :download_link, :embedded_code, :itunes, :google, :amazon, :spotify, :youtube)
+    end
+
 end
