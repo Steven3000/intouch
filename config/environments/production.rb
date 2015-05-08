b6283430e5b2f9d5abff22d95bda6bd15995a836
Steven Callen <wchi_2000@yahoo.com>
2015-05-08 18:29:22 -0500

    adding sendgrid settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,15 @@ Rails.application.configure do
     }
   }
 
+  config.action_mailer.default_url_options = { :host => "http://ntouchmusic.herokuapp.com" }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
