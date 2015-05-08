6b99f347964dcd049fb63d29f602896199f7f8fb
Steven Callen <wchi_2000@yahoo.com>
2015-05-08 18:18:13 -0500

    daniel fri eve next

---
+++ app/controllers/user_registrations_controller.rb
@@ -0,0 +1,10 @@
+class UserRegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, :only => [:create]
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'page#home'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "user_registrations" }
 
   resources :announcements
   resources :subscriptions
