35ad6f984d8d2e2f6eda2b8fd7cc41f5c59033f3
Steven Callen <wchi_2000@yahoo.com>
2015-05-12 17:39:02 -0500

    subscriptions fix

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,11 +5,6 @@ class Subscription < ActiveRecord::Base
 
   validates :user, :presence => true
 
-  has_attached_file :avatar,
-    :styles => { :medium => "600x600>", :thumb => "100x100>" },
-    :default_url => "/images/:style/missing.jpg"
-
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
    def picture_url
      "artists/#{name.parameterize}.jpg"
