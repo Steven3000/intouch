2d5be437032f218ad3cd61a95c3ad221dfd78b9d
Steven Callen <wchi_2000@yahoo.com>
2015-05-12 21:40:04 -0500

    seed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,11 +5,12 @@ class Subscription < ActiveRecord::Base
 
   validates :user, :presence => true
 
-  has_attached_file :avatar,
+  if has_attached_file :avatar,
     :styles => { :medium => "600x600>", :thumb => "100x100>" },
     :default_url => "/images/:style/missing.jpg"
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  end
 
    def picture_url
      "artists/#{self.name.parameterize}.jpg"
