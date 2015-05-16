02ba6b793d3761c4fd72ab6e17432af18e38aa6c
Steven Callen <wchi_2000@yahoo.com>
2015-05-16 14:46:53 -0500

    admin clean up forms css

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,7 +14,13 @@ class Artist < ActiveRecord::Base
     :styles => { :medium => "550x550>", :thumb => "200x200>" },
     :default_url => "/images/:style/missing.jpg"
 
+  has_attached_file :pic,
+    :styles => { :medium => "550x550>", :thumb => "200x200>" },
+    :default_url => "/images/:style/missing.jpg"
+
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
 
    def picture_url
      "artists/#{name.parameterize}.jpg"
