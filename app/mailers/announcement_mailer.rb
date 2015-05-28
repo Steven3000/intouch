31b2ec97e01fb636c70e5a8fcb13aa9b463fc61b
Steven Callen <wchi_2000@yahoo.com>
2015-05-27 23:06:16 -0500

    mailers working

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -4,13 +4,13 @@ class AnnouncementMailer < ApplicationMailer
     @song = song
     @user = user
 
-    mail(to: @user.email, subject: "#{@song.artist.name} just released #{@song.title}")
+    mail(to: @user.email, subject: "#{@song.artist.name} NEW MUSIC! - #{@song.title}")
   end
 
   def announce_album(user, ablbum)
     @song = song
     @user = user
 
-    mail(to: @user.email, subject: "#{@album.artist.name} just released #{@album.name}")
+    mail(to: @user.email, subject: "#{@album.artist.name} NEW MUSIC! - #{@album.name}")
   end
 end
