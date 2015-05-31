56a12c4709805b768ef69180a4d2db2dfe4890ea
Steven Callen <wchi_2000@yahoo.com>
2015-05-30 23:31:14 -0500

    announcement mailer fix

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -7,8 +7,8 @@ class AnnouncementMailer < ApplicationMailer
     mail(to: @user.email, subject: "#{@song.artist.name} - NEW MUSIC! - #{@song.title}")
   end
 
-  def announce_album(user, ablbum)
-    @song = song
+  def announce_album(user, album)
+    @album = album
     @user = user
 
     mail(to: @user.email, subject: "#{@album.artist.name} - NEW MUSIC! - #{@album.name}")
