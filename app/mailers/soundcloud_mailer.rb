8ca89138cc444f15e7ca6ab085a93d5a36d34de0
Steven Callen <wchi_2000@yahoo.com>
2015-06-11 22:49:34 -0500

    sc mailer fix

--- app/mailers/soundcloud_mailer.rb
+++ app/mailers/soundcloud_mailer.rb
@@ -2,7 +2,7 @@ class SoundcloudMailer < ApplicationMailer
 
   def soundcloud()
      #   @user = user
-    mail(to: wchi_2000@yahoo.com, subject: "SoundCloud API JOB")
+    # mail(to: wchi_2000@yahoo.com, subject: "SoundCloud API JOB")
   end
 
 end
