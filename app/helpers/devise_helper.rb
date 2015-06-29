acdcf543a2d6124de76809f54062a7fbd24ec0e4
steven callen <wchi_2000@yahoo.com>
2015-06-29 14:54:41 -0500

    devise helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,11 +3,16 @@ module DeviseHelper
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
     html = <<-HTML
-    <div class="alert alert-error alert-block"> <button type="button"
-    class="close" data-dismiss="alert">x</button>
-      #{messages}
-    </div>
+      <div id="error_explanation">
+        <h2>#{sentence}</h2>
+        <ul>#{messages}</ul>
+      </div>
     HTML
 
     html.html_safe
