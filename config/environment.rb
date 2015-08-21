f05a67f81e35eea5844c0299e538b2d5fc55dbb4
steven callen <wchi_2000@yahoo.com>
2015-08-21 12:13:43 -0500

    still aggravated working

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.omniauth :facebook, "APP_ID", "APP_SECRET"
+config.omniauth :facebook, "1602139400046031", "6f76314c68db7bb78d7a858350704301"
