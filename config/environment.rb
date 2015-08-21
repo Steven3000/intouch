0d46d1e0c93e289b663d0f0e2dc11dbd22d5cc12
steven callen <wchi_2000@yahoo.com>
2015-08-21 12:15:14 -0500

    fixing???

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.omniauth :facebook, "1602139400046031", "6f76314c68db7bb78d7a858350704301"
