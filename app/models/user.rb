e019ffcc0f692d44563b711baa14d9de2814c0e2
steven callen <wchi_2000@yahoo.com>
2015-08-24 13:06:45 -0500

    still trying...  models omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, uniqueness: {case_sesitive: false}
   validates :username, uniqueness: {case_sesitive: false}
